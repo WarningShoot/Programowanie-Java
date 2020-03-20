1101c523cd413e6eed25337733056da225b87fee
Damian Zajda <damian.zajda@unilodz.eu>
2020-03-20 16:47:48 +0100

    Kalkulator skonczony

--- src/damian/zajda/kalkulator/KalkulatorTemperatury.java
+++ src/damian/zajda/kalkulator/KalkulatorTemperatury.java
@@ -1,4 +1,13 @@
 package damian.zajda.kalkulator;
 
 public class KalkulatorTemperatury {
+    public double getFahrenheit(double celcious)
+    {
+        return 1.8 * celcious + 32.0;
+    }
+
+    public double getCelcious(double fahrenheit)
+    {
+        return ((fahrenheit - 32) * 5) / 9;
+    }
 }
--- src/damian/zajda/kalkulator/Program1.java
+++ src/damian/zajda/kalkulator/Program1.java
@@ -2,6 +2,12 @@ package damian.zajda.kalkulator;
 
 public class Program1 {
     public static void main(String[] args) {
+        KalkulatorTemperatury kalk = new KalkulatorTemperatury();
 
+        double celcious = 20.0; //Przykładowe dane
+        double fahrenheit = 100.0; //Przykładowe dane
+
+        System.out.println("Stopnie Fahrenheita " + kalk.getFahrenheit(celcious) + "F");
+        System.out.println("Stopnie Celciusza " + kalk.getCelcious(fahrenheit) + "C");
     }
 }
