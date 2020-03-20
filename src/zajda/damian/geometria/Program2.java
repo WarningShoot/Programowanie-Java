a2e11b596265b7eabadeb2d998badf05a727ba54
Damian Zajda <damian.zajda@unilodz.eu>
2020-03-20 17:16:56 +0100

    2 program skonmczony

--- src/zajda/damian/geometria/Program2.java
+++ src/zajda/damian/geometria/Program2.java
@@ -2,7 +2,19 @@ package zajda.damian.geometria;
 
 public class Program2 {
     public static void main(String[] args) {
-        Punkt3D pkt = new Punkt3D();
-        System.out.println(pkt.getX());
+        Punkt2D pkt2D = new Punkt2D(5, 10);
+        Punkt3D pkt3D = new Punkt3D(20, 30, 10);
+
+        System.out.println("Punkty 2D");
+        System.out.println("X: " + pkt2D.getX());
+        System.out.println("Y: " + pkt2D.getY());
+
+        System.out.println();
+
+        System.out.println("Punkty 3D");
+        System.out.println("X: " + pkt3D.getX());
+        System.out.println("Y: " + pkt3D.getY());
+        System.out.println("Z: " + pkt3D.getZ());
+
     }
 }
